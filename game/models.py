73f80eb32ade2e552b3af191f7eace916dcf0c83
StasMercer <pro100.stas.ru@gmail.com>
2018-10-27 12:53:53 +0300

    api v1.3

--- game/models.py
+++ game/models.py
@@ -7,6 +7,8 @@ class Answer(models.Model):
 
     text = models.TextField()
 
+    #character = models.OneToOneField('Character', default='0000000', on_delete=models.CASCADE, null=True)
+
     next_question = models.ForeignKey('Question',
                                       on_delete=models.CASCADE,
                                       related_name='question_in_a',
@@ -21,14 +23,19 @@ class Question(models.Model):
 
     background = models.CharField(max_length=100, null=True)
 
+    u_id = models.IntegerField(default='000', null=True)
+    #character = models.OneToOneField('Character', default='0000000', on_delete=models.CASCADE, null=True)
+
     text = models.TextField()
 
     answers = models.ManyToManyField('Answer',
                                      related_name='answer_in_q',
-                                     null=True,
                                      blank=True)
 
     def __str__(self):
         return self.text
+
+
+
 
 
